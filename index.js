94764337265d8b98f63f17b1392a510dea055155
prajwal9773 <125468457+prajwal9773@users.noreply.github.com>
2023-08-16 10:12:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1= Math.floor(Math.random()*6+1);
 var dice="dice" + randomNumber1 +".png";
-var images_left="images/" + dice;
+var images_left=dice;
 document.querySelectorAll(".img1")[0].setAttribute("src",images_left) ;
 var randomNumber2= Math.floor(Math.random()*6+1);
 var dice="dice" + randomNumber2 +".png";
-var images_right="images/" + dice;
+var images_right= dice;
 document.querySelectorAll(".img2")[0].setAttribute("src",images_right) ;
 if(randomNumber1===randomNumber2){
     document.querySelector("h1").textContent="Draw!";
