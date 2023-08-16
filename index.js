c2b5103da03ac0c995ce8e814cbb09dfc7df2fe4
prajwal9773 <125468457+prajwal9773@users.noreply.github.com>
2023-08-16 09:54:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1= Math.floor(Math.random()*6+1);
+var dice="dice" + randomNumber1 +".png";
+var images_left="images/" + dice;
+document.querySelectorAll(".img1")[0].setAttribute("src",images_left) ;
+var randomNumber2= Math.floor(Math.random()*6+1);
+var dice="dice" + randomNumber2 +".png";
+var images_right="images/" + dice;
+document.querySelectorAll(".img2")[0].setAttribute("src",images_right) ;
+if(randomNumber1===randomNumber2){
+    document.querySelector("h1").textContent="Draw!";
+}
+else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="Player 1 wins !!";
+}
+else{
+    document.querySelector("h1").textContent="Player 2 wins !!";
+}
